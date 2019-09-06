7d9dae22be54be5c1b9072f1ae9fb09fb6a22618
Rune Aasvestad <rune.aasvestad@capgemini.com>
2019-09-06 14:32:32 +0200

    Init of project

---
+++ pact-provider/src/main/java/no/runsoft/pactprovider/PactProviderApplication.java
@@ -0,0 +1,13 @@
+package no.runsoft.pactprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactProviderApplication.class, args);
+	}
+
+}
---
+++ pact-provider/src/test/java/no/runsoft/pactprovider/PactProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package no.runsoft.pactprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PactProviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
